850f52361db79d22d5c44f8e7f8b940c0ff7673a
Viktor1504 <viktordenisevich6@gmail.com>
2023-11-11 15:00:44 +0300

    CountTheDivisorsOfAnumber

---
+++ CountTheDivisorsOfAnumber.js
@@ -0,0 +1,9 @@
+const getDivisorsCnt = (n) => {
+    let result = 0;
+    for (let i = n; i > 0; i--) {
+        if (n % i === 0) result++
+    }
+    return result
+}
+
+console.log(getDivisorsCnt(10))
